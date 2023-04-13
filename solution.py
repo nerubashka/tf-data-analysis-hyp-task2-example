04761914830b90f1137ac3304598747e232fbbfc
Анастасия Нерубацкая <50240702+nerubashka@users.noreply.github.com>
2023-04-13 23:07:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 460109099 # Ваш chat ID, не меняйте название переменной
+alpha = 0.04
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_val = anderson_ksamp([x, y])[2]
+    return p_val < alpha # Ваш ответ, True или False
